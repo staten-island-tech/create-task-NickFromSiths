d5fa28968155f132757806ccd4119973cedd65c3
Nick <nmalyarchuk07@gmail.com>
2024-12-13 13:44:19 -0500

    .

---
+++ app/JS/main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import { chessPieces } from "./pieces.js";
+
+DOMSelectors = {
+  container: document.querySelector(".container"),
+};
---
+++ app/JS/pieces.js
@@ -0,0 +1,52 @@
+const chessPieces = [
+  {
+    piece: "White Rook",
+    img: "<img src='path_to_images/white_rook.png' alt='White Rook'>",
+  },
+  {
+    piece: "White Knight",
+    img: "<img src='path_to_images/white_knight.png' alt='White Knight'>",
+  },
+  {
+    piece: "White Bishop",
+    img: "<img src='path_to_images/white_bishop.png' alt='White Bishop'>",
+  },
+  {
+    piece: "White Queen",
+    img: "<img src='path_to_images/white_queen.png' alt='White Queen'>",
+  },
+  {
+    piece: "White King",
+    img: "<img src='path_to_images/white_king.png' alt='White King'>",
+  },
+  {
+    piece: "White Pawn",
+    img: "<img src='path_to_images/white_pawn.png' alt='White Pawn'>",
+  },
+  {
+    piece: "Black Rook",
+    img: "<img src='path_to_images/black_rook.png' alt='Black Rook'>",
+  },
+  {
+    piece: "Black Knight",
+    img: "<img src='path_to_images/black_knight.png' alt='Black Knight'>",
+  },
+  {
+    piece: "Black Bishop",
+    img: "<img src='path_to_images/black_bishop.png' alt='Black Bishop'>",
+  },
+  {
+    piece: "Black Queen",
+    img: "<img src='path_to_images/black_queen.png' alt='Black Queen'>",
+  },
+  {
+    piece: "Black King",
+    img: "<img src='path_to_images/black_king.png' alt='Black King'>",
+  },
+  {
+    piece: "Black Pawn",
+    img: "<img src='path_to_images/black_pawn.png' alt='Black Pawn'>",
+  },
+];
+
+export { chessPieces };
