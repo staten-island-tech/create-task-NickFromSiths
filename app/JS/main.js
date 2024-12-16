95f248eb8a1e58f17eaf043cfb5b7b43484859d8
Nick <nmalyarchuk07@gmail.com>
2024-12-16 14:02:30 -0500

    .

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,39 @@
 import "../css/style.css";
 import { chessPieces } from "./pieces.js";
+import { chessPiecesImg } from "./pieces.js";
 
-DOMSelectors = {
+const DOMSelectors = {
   container: document.querySelector(".container"),
+  board: document.querySelector(".middle70"),
+  a: document.querySelector("#a"),
+  b: document.querySelector("#b"),
+  c: document.querySelector("#c"),
+  d: document.querySelector("#d"),
+  e: document.querySelector("#e"),
+  f: document.querySelector("#f"),
+  g: document.querySelector("#g"),
+  h: document.querySelector("#h"),
+  a1: document.querySelector("#1,1"),
 };
+function testinput() {
+  chessPieces.forEach((element) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<img src="${element.img}" alt="">`
+    );
+  });
+}
+testinput();
+
+function resetboard() {
+  const wp = DOMSelectors.board.b.querySelectorAll("square", "square1");
+  wp.insertAdjacentHTML(
+    "beforeend",
+    `<img src="${chessPiecesImg.whitep}" alt="">`
+  );
+  DOMSelectors.a1.insertAdjacentHTML(
+    "beforeend",
+    `<img src="${chessPiecesImg.whitep}" alt="">`
+  );
+}
+resetboard();
--- app/JS/pieces.js
+++ app/JS/pieces.js
@@ -49,105 +49,106 @@
 //   },
 // ];
 
-// const chessPieces = [
-//   {
-//     piece: "White Rook",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png",
-//   },
-//   {
-//     piece: "White Knight",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
-//   },
-//   {
-//     piece: "White Bishop",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png",
-//   },
-//   {
-//     piece: "White Queen",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png",
-//   },
-//   {
-//     piece: "White King",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png",
-//   },
-//   {
-//     piece: "White Pawn",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png",
-//   },
-//   {
-//     piece: "Black Rook",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png",
-//   },
-//   {
-//     piece: "Black Knight",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png",
-//   },
-//   {
-//     piece: "Black Bishop",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png",
-//   },
-//   {
-//     piece: "Black Queen",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png",
-//   },
-//   {
-//     piece: "Black King",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png",
-//   },
-//   {
-//     piece: "Black Pawn",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png",
-//   },
-// ];
-
 const chessPieces = [
   {
-    "black pawn":
+    piece: "White Rook",
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png",
+  },
+  {
+    piece: "White Knight",
+    img: "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
+  },
+  {
+    piece: "White Bishop",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png",
+  },
+  {
+    piece: "White Queen",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png",
+  },
+  {
+    piece: "White King",
+    img: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png",
+  },
+  {
+    piece: "White Pawn",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png",
+  },
+  {
+    piece: "Black Rook",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png",
+  },
+  {
+    piece: "Black Knight",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png",
+  },
+  {
+    piece: "Black Bishop",
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png",
+  },
+  {
+    piece: "Black Queen",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png",
+  },
+  {
+    piece: "Black King",
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png",
+  },
+  {
+    piece: "Black Pawn",
+    img: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png",
+  },
+];
+
+const chessPiecesImg = [
+  {
+    blackp:
       "https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png",
   },
   {
-    "white pawn":
+    whitep:
       "https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png",
   },
   {
-    "black rook":
+    blackr:
       "https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png",
   },
   {
-    "white rook":
+    whiter:
       "https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png",
   },
   {
-    "black knight":
+    blackk:
       "https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png",
   },
   {
-    "white knight":
+    whitek:
       "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
   },
   {
-    "black bishop":
+    blackb:
       "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png",
   },
   {
-    "white bishop":
+    whiteb:
       "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png",
   },
   {
-    "black queen":
+    blackq:
       "https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png",
   },
   {
-    "white queen":
+    whiteq:
       "https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png",
   },
   {
-    "black king":
+    blackK:
       "https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png",
   },
   {
-    "white king":
+    whiteK:
       "https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png",
   },
 ];
 export { chessPieces };
+export { chessPiecesImg };
